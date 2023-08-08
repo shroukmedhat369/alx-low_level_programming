aa8e870f8dfd262e2fb41337434ead6fda249a2e
shroukmedhat369 <shroukmedhat59@gmail.com>
2023-08-08 15:54:57 -0700

    11

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,13 +5,10 @@
 #include "main.h"
 
 /**
- * _strdup - duplicate to new memory space location
-
- * @str: char
-
- * Return: 0
-
- */
+* _strdup - duplicate to new memory space location
+* @str: char
+* Return: 0
+*/
 
 char *_strdup(char *str)
 
